a9c7d173e254cde050caf88f0a84331bdb726f01
hiramramirez <hiram.j.ramirez@gmail.com>
2023-02-25 04:07:48 -0600

    las rondas estan funcionando

--- generador.js
+++ generador.js
@@ -1,37 +1,59 @@
 const round = document.querySelector('#round');
+const verificar = document.querySelector('#verificar');
 
-const millar1 = document.querySelector('#millar1');
-const centena1 = document.querySelector('#centena1');
-const decena1 = document.querySelector('#decena1');
 const unidad1 = document.querySelector('#unidad1');
-
-const millar2 = document.querySelector('#millar2');
-const centena2 = document.querySelector('#centena2');
-const decena2 = document.querySelector('#decena2');
 const unidad2 = document.querySelector('#unidad2');
 
-const respuesta = document.querySelector('#respuesta');
 
-let contador = 0;
-let resultado = null
+let contador = 1;
 
 round.addEventListener('click', () => {
-  millar1.innerHTML = crearNumeroRandom();
-  centena1.innerHTML = crearNumeroRandom();
-  decena1.innerHTML = crearNumeroRandom();
-  unidad1.innerHTML = crearNumeroRandom();
 
-  millar2.innerHTML = crearNumeroRandom();
-  centena2.innerHTML = crearNumeroRandom();
-  decena2.innerHTML = crearNumeroRandom();
-  unidad2.innerHTML = crearNumeroRandom();
+  console.log(`Ronda: ${contador}`);
 
-  console.log(unidad1.innerHTML, unidad2.innerHTML)
-  console.log(Number(unidad1.innerHTML) + Number(unidad2.innerHTML))
+  if (contador === 1) {
+    unidad2.innerHTML = crearNumeroRandom1();
+    unidad1.innerHTML = crearNumeroRandom1();
+  } else if (contador === 2) {
+    unidad2.innerHTML = crearNumeroRandom2();
+    unidad1.innerHTML = crearNumeroRandom2();
+  } else if (contador === 3) {
+    unidad2.innerHTML = crearNumeroRandom3();
+    unidad1.innerHTML = crearNumeroRandom3();
+  } else if (contador === 4) {
+    unidad2.innerHTML = crearNumeroRandom4();
+    unidad1.innerHTML = crearNumeroRandom4();
+  }
 
-  // millar1.classList.remove('noMostrar');
+  resultado();
+  console.log(resultado());
+
 })
 
-crearNumeroRandom = () => {
+verificar.addEventListener('click', () => {
+  if (resultado() == respuesta.value) {
+    console.log('Correcto');
+    contador++
+  } else {
+    console.log('Incorrecto');
+  }
+});
+
+crearNumeroRandom1 = () => {
   return Math.floor(Math.random() * 9) + 1;
+}
+crearNumeroRandom2 = () => {
+  return Math.floor(Math.random() * 99) + 1;
+}
+crearNumeroRandom3 = () => {
+  return Math.floor(Math.random() * 999) + 1;
+}
+crearNumeroRandom4 = () => {
+  return Math.floor(Math.random() * 9999) + 1;
+}
+
+resultado = (a, b) => {
+  a = Number(unidad1.innerHTML);
+  b = Number(unidad2.innerHTML);
+  return a + b;
 }
